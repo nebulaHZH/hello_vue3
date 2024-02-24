8c198c573b4717d20b0d0c6ff9440a1384eb03bf
username <3113613254@qq.com>
2024-02-24 11:51:36 +0800

    first commit

---
+++ note.md
@@ -0,0 +1,2 @@
+npm install -> 按照依赖包（ts等模块）
+![alt text](image.png) ->运行名dev
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './assets/main.css'//导入样式
+
+import { createApp } from 'vue' //创建应用
+import App from './App.vue' //Vue原始组件
+
+createApp(App).mount('#app')
